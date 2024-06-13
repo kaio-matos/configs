028954a9f957c0744f4ebc173f5c282c271a4bd1
Kaio Matos <72654258+kaio-matos@users.noreply.github.com>
2024-06-13 20:45:14 -0300

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -532,7 +532,8 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        -- https://stackoverflow.com/questions/74785927/clangd-doesnt-recognize-standard-headers
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},

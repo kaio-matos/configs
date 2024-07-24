df573d043639c7beacee13a63c2dda99a1df3377
Kaio Matos <kaio_matos22@hotmail.com>
2024-07-23 21:03:30 -0300

    add nvim-autopairs plugin

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -67,4 +67,12 @@ return {
       },
     },
   },
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
 }

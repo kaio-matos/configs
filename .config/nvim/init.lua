d44b57ecee712b3685d00b88f1e3f6f1942ea21d
Kaio Matos <kaio_matos22@hotmail.com>
2024-08-05 09:27:48 -0300

    update nvim tree keys

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,6 +157,13 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,18 +1,22 @@
 local function nvim_tree_on_attach(bufnr)
   local api = require 'nvim-tree.api'
 
-  -- local function opts(desc)
-  --   return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  -- end
+  local function opts_overwrite(desc)
+    return { desc = 'nvim-tree: ' .. desc }
+  end
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, silent = true, nowait = true }
+  end
 
   -- default mappings
   api.config.mappings.default_on_attach(bufnr)
 
   -- custom mappings
   -- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts 'Up')
-  -- vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
-  vim.keymap.set('n', '<leader>a', '<cmd>NvimTreeFocus<cr>', { desc = 'Focus Explorer' })
-  vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle Explorer' })
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+  vim.keymap.set('n', '<leader>a', '<cmd>NvimTreeFocus<cr>', opts_overwrite 'Focus Explorer')
+  vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<cr>', opts_overwrite 'Toggle Explorer')
 end
 
 -- You can add your own plugins here or in other files in this directory!

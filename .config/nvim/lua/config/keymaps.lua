ddbd2d0b3b53a68b4a2c12be13c75ec8bfa1da24
Kaio Matos <kaio_matos22@hotmail.com>
2025-04-08 12:14:06 -0300

    migrate to lazyvim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "<C-b>", "<ESC>^i", { desc = "move beginning of line" })
+vim.keymap.set("i", "<C-e>", "<End>", { desc = "move end of line" })
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "move left" }) -- conflicts with some keybindings from nvim-cmp (but i prefer this so its commented)
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "move right" }) -- conflicts with some keybindings from nvim-cmp (but i prefer this so its commented)
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "move down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "move up" })
+
+vim.keymap.set("t", "<C-x>", "<C-\\><C-N>", { desc = "Terminal escape terminal mode", noremap = true })
+
+--- Tab and Shift Tab
+local opts = { noremap = true, silent = true }
+vim.keymap.set("v", "<Tab>", ">gv", opts)
+vim.keymap.set("v", "<S-Tab>", "<gv", opts)
+vim.keymap.set("i", "<S-Tab>", "C-d")
---
+++ .config/nvim/readme.md
@@ -0,0 +1,5 @@
+Added fzf and chafa
+
+`brew install fzf`
+
+`sudo apt install chafa`

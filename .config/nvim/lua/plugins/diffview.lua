4d9c28dd6e392876ecfb5d8939ade5bc7d16a33d
Kaio Matos <kaio_matos22@hotmail.com>
2025-04-08 16:01:43 -0300

    add diffview and some other quality of life plugins

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>gd", function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end, { desc = "Diff View" })
+    end,
+  },
+}

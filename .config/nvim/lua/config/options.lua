d627e080410cb1ddf75e757bdb9dd0ffec921ae7
Kaio Matos <kaio_matos22@hotmail.com>
2025-06-09 11:31:16 -0300

    use 4 tabs minimum

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local o = vim.o
+
+o.expandtab = true -- expand tab input with spaces characters
+o.smartindent = true -- syntax aware indentations for newline inserts
+o.tabstop = 4 -- num of space characters per tab
+o.shiftwidth = 4 -- spaces per indentation level

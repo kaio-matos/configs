ac3bdcb997aa216694b103e765fb3ed6b164df53
Kaio Matos <kaio_matos22@hotmail.com>
2025-06-09 12:13:34 -0300

    start using snacks picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,14 @@ return {
     ---@type snacks.Config
     opts = {
       image = { enabled = true },
+      picker = {
+        enabled = true,
+        formatters = {
+          file = {
+            truncate = 100, -- truncate the file path to (roughly) this length
+          },
+        },
+      },
     },
   },
 }

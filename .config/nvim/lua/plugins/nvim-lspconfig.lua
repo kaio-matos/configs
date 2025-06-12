abbade0ca5b1b08cb0f120f073bff2e60e4f066f
Kaio Matos <kaio_matos22@hotmail.com>
2025-06-12 19:49:06 -0300

    disable inlay hints by default

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,15 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function(opts)
-      -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- keys[#keys + 1] = { "C-k", false } -- Disable
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+    -- opts = function(opts)
+    -- local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    -- keys[#keys + 1] = { "C-k", false } -- Disable
 
-      opts.inlay_hints = { enabled = false }
-    end,
+    -- opts.inlay_hints = { enabled = false } -- dont work
+    -- return opts
+    -- end,
   },
 }

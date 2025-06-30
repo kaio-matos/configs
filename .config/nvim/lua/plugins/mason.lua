90b05011691691bfc5eb0b3bfc65e8ecc5a49ed0
Kaio Matos <kaio_matos22@hotmail.com>
2025-06-30 14:26:21 -0300

    add ensure installed for mason

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "astro-language-server",
+        "codelldb",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "hadolint",
+        "json-lsp",
+        "lua-language-server",
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+        "prettier",
+        "shfmt",
+        "stylua",
+        "tailwindcss-language-server",
+        "taplo",
+        "vtsls",
+        "vue-language-server",
+        "wgsl-analyzer",
+        "yaml-language-server",
+      },
+    },
+  },
+}

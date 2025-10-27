153af2121c6e5b29518379d5502474805eae7f53
Kaio Matos <kaio_matos22@hotmail.com>
2025-10-27 14:48:08 -0300

    update lazyvim and its dependencies and remove copilot-chat

--- .config/nvim/lua/plugins/tailwind.lua
+++ .config/nvim/lua/plugins/tailwind.lua
@@ -1,52 +1,54 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tailwindcss = {
-          -- exclude a filetype from the default_config
-          filetypes_exclude = {},
-          -- add additional filetypes to the default_config
-          filetypes_include = {
-            "rust",
-          },
-          -- to fully override the default_config, change the below
-          -- filetypes = {}
-        },
-      },
-      setup = {
-        tailwindcss = function(_, opts)
-          local tw = LazyVim.lsp.get_raw_config("tailwindcss")
-          opts.filetypes = opts.filetypes or {}
-
-          -- Add default filetypes
-          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
-
-          -- Remove excluded filetypes
-          --- @param ft string
-          opts.filetypes = vim.tbl_filter(function(ft)
-            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-          end, opts.filetypes)
-
-          -- Additional settings for Phoenix projects
-          opts.settings = {
-            tailwindCSS = {
-              includeLanguages = {
-                rust = "html",
-                elixir = "html-eex",
-                eelixir = "html-eex",
-                heex = "html-eex",
-              },
-              experimental = {
-                classRegex = { 'class\\s*:\\s*"([^"]*)' },
-              },
-            },
-          }
-
-          -- Add additional filetypes
-          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-        end,
-      },
-    },
-  },
+  -- TODO: Fix
+  -- The LazyVim.lsp.get_raw_config doesn't exist in the new version
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       tailwindcss = {
+  --         -- exclude a filetype from the default_config
+  --         filetypes_exclude = {},
+  --         -- add additional filetypes to the default_config
+  --         filetypes_include = {
+  --           "rust",
+  --         },
+  --         -- to fully override the default_config, change the below
+  --         -- filetypes = {}
+  --       },
+  --     },
+  --     setup = {
+  --       tailwindcss = function(_, opts)
+  --         local tw = LazyVim.lsp.get_raw_config("tailwindcss")
+  --         opts.filetypes = opts.filetypes or {}
+  --
+  --         -- Add default filetypes
+  --         vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+  --
+  --         -- Remove excluded filetypes
+  --         --- @param ft string
+  --         opts.filetypes = vim.tbl_filter(function(ft)
+  --           return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+  --         end, opts.filetypes)
+  --
+  --         -- Additional settings for Phoenix projects
+  --         opts.settings = {
+  --           tailwindCSS = {
+  --             includeLanguages = {
+  --               rust = "html",
+  --               elixir = "html-eex",
+  --               eelixir = "html-eex",
+  --               heex = "html-eex",
+  --             },
+  --             experimental = {
+  --               classRegex = { 'class\\s*:\\s*"([^"]*)' },
+  --             },
+  --           },
+  --         }
+  --
+  --         -- Add additional filetypes
+  --         vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+  --       end,
+  --     },
+  --   },
+  -- },
 }
